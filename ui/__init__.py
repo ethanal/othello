e2234d41831d513b4da17d1031e2856785d23089
Ethan Lowman <ethan.l.527@gmail.com>
2014-12-11 13:53:50 -0500

    Remove unused UI event pipe

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,11 +1,6 @@
-import multiprocessing as mp
-
-
 class UI:
 
     def __init__(self, game):
-        parent_conn, child_conn = mp.Pipe(duplex=False)
-        self.ui_event_pipe = parent_conn
         self.game = game
 
     def get_move(self):
