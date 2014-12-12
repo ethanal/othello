7d442a600a4875cc6ea39dcc2404ea96a265a0af
Ethan Lowman <ethan.l.527@gmail.com>
2014-12-12 09:07:45 -0500

    Add gitignore to players directory

--- players/AI.py
+++
@@ -1,9 +0,0 @@
-from . import Player
-from time import sleep
-
-
-class AI(Player):
-
-    def move(self, board):
-        sleep(5)
-        return board.legal_moves(self.color)[0]
--- players/ExampleAI.py
+++ players/ExampleAI.py
@@ -1,10 +1,12 @@
 from random import choice
+from time import sleep
 from . import Player
 
 
-class Random(Player):
+class ExampleAI(Player):
 
     def move(self, board):
+        sleep(5)
         moves = board.legal_moves(self.color)
         if len(moves) != 0:
             return choice(moves)
